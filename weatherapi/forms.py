5ec500c1ebb092e09c09b7a1b2511d8c5aad43d9
Avukonke Peter <avu@gradconnection.com>
2020-08-07 12:14:30 +0200

    Register urls, forms for use in views
    #weatherapi

--- weatherapi/forms.py
+++ weatherapi/forms.py
@@ -1,5 +1,9 @@
+from datetime import datetime, timedelta
+
+
 from django import forms
 from django.forms import widgets
+from django.conf import settings
 
 
 from weatherapi.models import WeatherAPI
@@ -8,12 +12,13 @@ from weatherapi.models import WeatherAPI
 class WeatherAPIForm(forms.ModelForm):
 
     class Meta:
+        model = WeatherAPI
         fields = '__all__'
 
 
 class LandingPageForm(forms.Form):
     city = forms.CharField(
-        required=True, help_text="The city to search"
+        required=True, help_text="The city to search for"
     )
     weather_apis = forms.ModelMultipleChoiceField(
         queryset=WeatherAPI.objects.filter(userapi=True).all(),
@@ -21,10 +26,30 @@ class LandingPageForm(forms.Form):
         help_text="The weather API's to query"
     )
     start_date = forms.DateField(
-        help_text="Please enter the end_date for your query",
+        help_text="Please enter the start_date for your query | Format: 'YYYY-MM-DD' | Default: 'Todays date'",
+        widget=widgets.DateInput(
+            format='%Y-%m-%d',
+            attrs={
+                'placeholder': 'YYYY-MM-DD',
+                'value': datetime.strftime(
+                    datetime.today(),
+                    format='%Y-%m-%d'
+                )
+            }
+        )
     )
     end_date = forms.DateField(
-        help_text="Please enter the end date for your query"
+        help_text="Please enter the end date for your query | Format: 'YYYY-MM-DD' | Default: 'One week into the future'",
+        widget=widgets.DateInput(
+            format='%Y-%m-%d',
+            attrs={
+                'placeholder': 'YYYY-MM-DD',
+                'value': datetime.strftime(
+                    datetime.today() + timedelta(days=settings.DEFAULT_QUERY_DURATION_DAYS),
+                    format='%Y-%m-%d'
+                )
+            }
+        )
     )
 
     class Meta:
--- weatherapi/urls.py
+++ weatherapi/urls.py
@@ -8,13 +8,17 @@ from rest_framework.routers import DefaultRouter
 from weatherapi import views
 
 router = DefaultRouter()
-router.register(prefix=r'weatherview', viewset=views.WeatherView, \
-                       basename='weatherview')
+router.register(
+    prefix=r'weatherview',
+    viewset=views.WeatherView,
+    basename='weatherview'
+)
 router.register(r'cityautocomplete', views.CityAutocomplete, basename='cityautocomplete')
 
 urlpatterns = [
     path('weatherhistory/', views.WeatherHistoryView.as_view(), name="weather-history"),
     path('admin/', admin.site.urls),
-    url(r'^api/', include(router.urls)),
-    path('', views.LandingPage.as_view())
+    path(r'api/weatherview', views.WeatherView.as_view(), name="weatherview"),
+    path(r'api/', include(router.urls)),
+    path('', views.LandingPage.as_view(), name='landing-page')
 ]
