57dae869a947f2a2fe39b63126ded89e3a541ccb
Avukonke Peter <avu@gradconnection.com>
2020-08-07 12:23:47 +0200

    DB
    #weatherapi

--- weatherapi/settings.py
+++ weatherapi/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'weatherapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'weatherapi2.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'weatherapi.sqlite3'),
     }
 }
 
